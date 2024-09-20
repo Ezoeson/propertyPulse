4b61c5499e69336ab1ce1fc6cb0798a1829e8676
Hasina ezoeson <nambinintsoa6017@gmail.com>
2024-09-20 13:24:04 +0300

    insertion de condition sur la page search-results

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -38,33 +38,36 @@ const SearchResultsPage = () => {
     };
     fetchSearchResults();
   }, [location, propertyType]);
-  console.log(properties);
+
+ 
 
   return (
-    <>
-      <section className='bg-gradient-to-tr from-black to-red-700 py-4'>
-        <div className='max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6  lg:px-8'>
-          <PropertySearchForm />
-        </div>
-      </section>
-      {loading ? (
-        <Spinner loading={loading} />
-      ) : (
-        <section className='px-4 py-6'>
-          <div className='container-xl lg:container m-auto px-4 py-6'>
-            {properties.length === 0 ? (
-              <p>No properties found</p>
-            ) : (
-              <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-                {properties?.map((property) => (
-                  <PropertyCard key={property._id} property={property} />
-                ))}
-              </div>
-            )}
+    properties?.length > 0 && (
+      <>
+        <section className='bg-gradient-to-tr from-black to-red-700 py-4'>
+          <div className='max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6  lg:px-8'>
+            <PropertySearchForm />
           </div>
         </section>
-      )}
-    </>
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <section className='px-4 py-6'>
+            <div className='container-xl lg:container m-auto px-4 py-6'>
+              {properties.length === 0 ? (
+                <p>No properties found</p>
+              ) : (
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+                  {properties?.map((property) => (
+                    <PropertyCard key={property._id} property={property} />
+                  ))}
+                </div>
+              )}
+            </div>
+          </section>
+        )}
+      </>
+    )
   );
 };
 
